30a1af6b8d499de918d477fd7349d2bfae152356
yonarw@gmail.com <yonarw@gmail.com>
2022-01-16 22:45:35 +0100

    Initial commit

---
+++ service.py
@@ -0,0 +1,31 @@
+import json
+import sensor_read
+import threading
+import time
+#import bme280
+
+from flask import Flask, request
+
+sensor_read.setupGPIO()
+curr_values = sensor_read.getWeatherInfo()
+
+def updateWinfo():
+    global curr_values
+    while True:
+        curr_values = sensor_read.getWeatherInfo()
+        time.sleep(10*1)
+
+def run_rest():
+    app = Flask(__name__)
+
+    @app.route('/sensor/state', methods=['GET'])
+    def get_state():
+        return json.dumps(curr_values)+"\n"
+    print("Starting flask app!")
+    app.run(host="0.0.0.0", port=5000)
+
+if __name__ == "__main__":
+    print("starting service!!!")
+    t = threading.Thread(target = updateWinfo)
+    t.start()
+    run_rest()
